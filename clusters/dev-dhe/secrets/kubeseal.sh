aeffaf5fd532e656ab4d7bd3797147bb5cc47f9b
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-07-03 00:37:29 +0200

    chore: created new valid secret for dev-dhe teleport-kube-agent

--- clusters/dev-dhe/secrets/kubeseal.sh
+++ clusters/dev-dhe/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-dev-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/security/keycloak/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/security/keycloak/postgres-config-keycloak.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/connectivity/keycloak/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/connectivity/teleport-agent/teleport-kube-agent-join-token.yaml
