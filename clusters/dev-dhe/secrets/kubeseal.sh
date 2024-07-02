473483bc34c771d4c511da001fda828695028302
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-07-02 10:33:00 +0200

    chore: adding secret for keycloak

--- clusters/dev-dhe/secrets/kubeseal.sh
+++ clusters/dev-dhe/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-dev-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/projects/incubator/gameservers/rcon/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/projects/incubator/gameservers/rcon/rcon-minecraft-server-password.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/security/keycloak/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/incubator/security/keycloak/postgres-config-keycloak.yaml
