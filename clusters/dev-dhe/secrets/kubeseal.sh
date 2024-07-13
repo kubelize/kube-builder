accd8237d36b638146f94ea7b8eb5456b48f7b0b
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-07-13 16:20:32 +0200

    chore: added alertmanager.yml as secret

--- clusters/dev-dhe/secrets/kubeseal.sh
+++ clusters/dev-dhe/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-dev-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/gitops/renovate/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/gitops/renovate/renovate-env.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/observability/kube-prometheus-stack/alertmanager.yml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/observability/kube-prometheus-stack/alertmanager-config.yaml
