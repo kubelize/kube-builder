17ddf0cccfed6d849b5d7333b0eacb0de28f4735
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-07-15 18:03:31 +0200

    chore: testing new configurationfile for alertmanager dev-dhe

--- clusters/dev-dhe/secrets/kubeseal.sh
+++ clusters/dev-dhe/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-dev-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/observability/kube-prometheus-stack/secret.yaml \
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/observability/kube-prometheus-stack/alertmanager-config.yml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/dev-dhe/secrets/operators/stable/observability/kube-prometheus-stack/alertmanager-config.yaml
