89de31072bee60bdc168444304180f0c8dbdd43f
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-09-14 11:46:58 +0200

    chore: added join token to standalone

--- clusters/standalone/secrets/kubeseal.sh
+++ clusters/standalone/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-standalone-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/projects/incubator/gameservers/minecraft/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/projects/incubator/gameservers/minecraft/minecraft-rcon-password.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/connectivity/teleport-agent/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/connectivity/teleport-agent/teleport-kube-agent-join-token.yaml
