0a3d655b28cabbe40f6c2ed5da409d53c89e1d0b
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-09-14 12:05:14 +0200

    chore: added alertmanager config to standalone

--- clusters/standalone/secrets/kubeseal.sh
+++ clusters/standalone/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=pub-standalone-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/connectivity/teleport-agent/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/connectivity/teleport-agent/teleport-kube-agent-join-token.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/observability/kube-prometheus-stack/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/standalone/secrets/operators/stable/observability/kube-prometheus-stack/alertmanager-config.yaml
