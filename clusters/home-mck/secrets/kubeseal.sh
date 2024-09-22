ca70d4f811bfa4f34de22599a93c33b5d5681288
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-09-22 17:56:06 +0200

    chore: added teleport-join token for home-mck

--- clusters/home-mck/secrets/kubeseal.sh
+++ clusters/home-mck/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
-  --cert=pub-prod-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/prod-mck/secrets/projects/incubator/gameservers/minecraft/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/prod-mck/secrets/projects/incubator/gameservers/minecraft/minecraft-rcon-password.yaml
+  --cert=pub-home-mck-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-mck/secrets/incubator/connectivity/teleport-agent/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-mck/secrets/incubator/connectivity/teleport-agent/teleport-kube-agent-join-token.yaml
