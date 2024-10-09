435b662d9d2c1ed5aeff5b9db8fb7d3d9661f9f5
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-10-09 14:02:21 +0200

    chore: fixed key in alertmanager-config

--- apps/addons/sealed-secrets/home-dhe/kubeseal.sh
+++ apps/addons/sealed-secrets/home-dhe/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/alertmanager-home-dhe.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-dhe.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-dhe/alertmanager-secret.yaml
--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-mck.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-mck/alertmanager-secret.yaml
