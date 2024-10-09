66a52cb74f52fee8daa4164bc414a497a431d9b2
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-10-09 10:14:10 +0200

    chore: added renovate-env secret

--- clusters/home-dhe/secrets/kubeseal.sh
+++ clusters/home-dhe/secrets/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
-  --cert=pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-dhe/secrets/incubator/connectivity/teleport-kube-agent/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-dhe/secrets/incubator/connectivity/teleport-kube-agent/teleport-kube-agent-join-token.yaml
+  --cert=/home/dan/Git/kubelize/kube-builder/clusters/home-dhe/secrets/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-dhe/secrets/stable/observability/renovate/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/home-dhe/secrets/stable/observability/renovate/renovate-env.yaml
