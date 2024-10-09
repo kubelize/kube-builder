c2f8292ef809dc5d315f8023db4613378ae53ffe
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-10-09 13:49:40 +0200

    chore: moved teleport-kube-agent join token to appropriate bootstrap

--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -1,4 +1,4 @@
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret2.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/alertmanager-home-mck.yaml
+  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
