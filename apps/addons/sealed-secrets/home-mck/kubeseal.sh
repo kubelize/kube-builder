22fd0a443edf4a3e78ad0dcac425a9da7d58e709
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-11-05 17:49:26 +0100

    chore: using new key to encrypt teleport-kube-agent-join-token for home-mck

--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -2,3 +2,8 @@ kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
+
+
+
+# Get the public key from the cluster
+kubeseal --fetch-cert --controller-namespace=sealed-secrets > /home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem
