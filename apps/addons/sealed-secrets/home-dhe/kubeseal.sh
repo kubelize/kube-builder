ab7ae9a52916d864ec46f77df191538296b0ff71
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-11-20 16:25:28 +0100

    chore: replacing all secrets for home-dhe

--- apps/addons/sealed-secrets/home-dhe/kubeseal.sh
+++ apps/addons/sealed-secrets/home-dhe/kubeseal.sh
@@ -2,3 +2,8 @@ kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-dhe.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-dhe/alertmanager-secret.yaml
+
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file apps/addons/teleport-kube-agent/join-token/home-dhe/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/teleport-kube-agent-join-token.yaml
