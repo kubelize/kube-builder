be05bc75fa1a404fa0bb342ac8a25d3c8f117d46
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-11-25 21:45:15 +0100

    chore: added secret for renovate

--- apps/addons/sealed-secrets/home-dhe/kubeseal.sh
+++ apps/addons/sealed-secrets/home-dhe/kubeseal.sh
@@ -1,9 +1,17 @@
+# alertmanager config
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-dhe.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-dhe/alertmanager-secret.yaml
 
+# teleport-kube-agent join token
 kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
   --secret-file apps/addons/teleport-kube-agent/join-token/home-dhe/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/teleport-kube-agent-join-token.yaml
+
+# renovate bot token
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file apps/operators/stable/gitops/renovate/deploy/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/operators/stable/gitops/renovate/deploy/renovate-env.yaml
