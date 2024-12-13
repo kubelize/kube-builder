626207471974b32078228be519e36ad7c52ceb82
Daniel Hendricken <daniel.hendricken@bedag.ch>
2024-12-13 10:06:51 +0100

    chore: removed uptime-kuma from kube-operators

--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -12,3 +12,9 @@ kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
   --secret-file apps/addons/teleport-kube-agent/join-token/home-mck/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
+
+# minecraft rcon password
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/minecraft-rcon-password.yaml
