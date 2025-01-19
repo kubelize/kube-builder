31a7b9c275de447385359363e2fb5809f05e6c12
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-01-19 02:29:10 +0100

    chore: adding etcd-backup s3-bucket

--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -18,3 +18,9 @@ kubeseal --format=yaml \
   --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
   --secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/secret.yaml \
   --sealed-secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/minecraft-rcon-password.yaml
+
+# etcd-backup
+kubeseal --format=yaml \
+  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/secret.yaml \
+  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-mck/env-config.yaml
