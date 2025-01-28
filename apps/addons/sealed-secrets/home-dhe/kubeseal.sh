8d44bd83bbdb972a60e3d80cd0fad1657788f66e
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-01-28 21:25:12 +0100

    chore: adjusted all minio tenant secrets

--- apps/addons/sealed-secrets/home-dhe/kubeseal.sh
+++ apps/addons/sealed-secrets/home-dhe/kubeseal.sh
@@ -1,29 +1,29 @@
 # Fetch the public key from the sealed-secrets controller
-kubeseal --fetch-cert --controller-namespace=sealed-secrets > /home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem
+kubeseal --fetch-cert --controller-namespace=sealed-secrets > /Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem
 
 # alertmanager config
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-dhe.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-dhe/alertmanager-secret.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-dhe.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-dhe/alertmanager-secret.yaml
 
 # teleport-kube-agent join token
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/teleport-kube-agent-join-token.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/secret.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-dhe/teleport-kube-agent-join-token.yaml
 
 # renovate bot token
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/operators/stable/gitops/renovate/deploy/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/operators/stable/gitops/renovate/deploy/renovate-env.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/operators/stable/gitops/renovate/deploy/secret.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/operators/stable/gitops/renovate/deploy/renovate-env.yaml
 
 # etcd-backup minio tenant
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backups/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backups/env-config.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-dhe/pub-home-dhe-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-dhe/secret.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-dhe/env-config.yaml
 
 # longhorn-backup minio tenant
 kubeseal --format=yaml \
--- apps/addons/sealed-secrets/home-mck/kubeseal.sh
+++ apps/addons/sealed-secrets/home-mck/kubeseal.sh
@@ -1,29 +1,29 @@
 # Fetch the public key from the sealed-secrets controller
-kubeseal --fetch-cert --controller-namespace=sealed-secrets > /home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem
+kubeseal --fetch-cert --controller-namespace=sealed-secrets > /Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem
 
 # alertmanager config
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-mck.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-mck/alertmanager-secret.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/secret-mck.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/alertmanager-config/home-mck/alertmanager-secret.yaml
 
 # teleport-kube-agent join token
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
   --secret-file apps/addons/teleport-kube-agent/join-token/home-mck/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/teleport-kube-agent/join-token/home-mck/teleport-kube-agent-join-token.yaml
 
 # minecraft rcon password
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/minecraft-rcon-password.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/secret.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-projects/base/stable/gameservers/minecraft-vanilla/minecraft-rcon-password.yaml
 
 # etcd-backup
 kubeseal --format=yaml \
-  --cert=/home/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-mck/env-config.yaml
+  --cert=/Users/dan/Git/kubelize/kube-builder/apps/addons/sealed-secrets/home-mck/pub-home-mck-sealed-secrets.pem \
+  --secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-mck/secret.yaml \
+  --sealed-secret-file /Users/dan/Git/kubelize/kube-builder/apps/addons/storage/minio-tenants/etcd-backup/home-mck/env-config.yaml
 
 # longhorn-backup
 kubeseal --format=yaml \
