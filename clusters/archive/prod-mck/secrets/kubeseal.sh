383a0f7ab6c0ce47aea4f5440f7e5c8d1d8bf5fa
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-03-29 14:10:35 +0100

    chore(cleanup): removing unused files

--- clusters/archive/prod-mck/secrets/kubeseal.sh
+++
@@ -1,4 +0,0 @@
-kubeseal --format=yaml \
-  --cert=pub-prod-mck-sealed-secrets.pem \
-  --secret-file /home/dan/Git/kubelize/kube-builder/clusters/prod-mck/secrets/projects/incubator/gameservers/minecraft/secret.yaml \
-  --sealed-secret-file /home/dan/Git/kubelize/kube-builder/clusters/prod-mck/secrets/projects/incubator/gameservers/minecraft/minecraft-rcon-password.yaml
